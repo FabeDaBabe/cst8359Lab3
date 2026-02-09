381f86af2b32ba6601a61a939fb974f2f31ad340
FabeDaBabe <fabian77naibaf@gmail.com>
2026-02-09 16:24:37 -0500

    Lab 3 not quite done init

---
+++ Controllers/WorkshopsController.cs
@@ -0,0 +1,14 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class WorkshopsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var rsvp = new Rsvp();
+            return View(rsvp);
+        }
+    }
+}
---
+++ Models/Rsvp.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab3.Models
+{
+    public class Rsvp
+    {
+        public string FullName { get; set; } = string.Empty;
+        public bool NeedsAccommodation { get; set; }
+        public string WorkshopTitle { get; set; } = string.Empty;
+    }
+}
