65d8bff35bb2eb7648193ab69f4320e1e420e6af
FabeDaBabe <fabian77naibaf@gmail.com>
2026-02-13 18:07:48 -0500

    Lab 3 done

--- Controllers/WorkshopsController.cs
+++ Controllers/WorkshopsController.cs
@@ -5,10 +5,35 @@ namespace Lab3.Controllers
 {
     public class WorkshopsController : Controller
     {
+        private static List<Rsvp> _registrations = new List<Rsvp>();
+
         public IActionResult Index()
         {
             var rsvp = new Rsvp();
             return View(rsvp);
+        }
+
+        public IActionResult RsvpForm()
+        {
+            return View(new Rsvp());
+        }
+
+        [HttpPost]
+        public IActionResult Confirm(Rsvp model)
+        {
+            if (ModelState.IsValid)
+            {
+                _registrations.Add(model);
+                ViewData["Message"] = $"Thanks for registering, {model.FullName}!";
+                return View(model);
+            }
+
+            return View("RsvpForm", model);
+        }
+
+        public IActionResult Registrations()
+        {
+            return View(_registrations);
         }
     }
 }
--- obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+381f86af2b32ba6601a61a939fb974f2f31ad340")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
